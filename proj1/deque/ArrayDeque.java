a13da915788d26da06725e21d33830f807ed8e66
Muzan Kibutsuji <2982287566@qq.com>
2025-11-18 20:43:53 +0800

    OK兄弟们改完了

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -11,13 +11,12 @@ public class ArrayDeque<T> implements Iterable<T> {
 
     private boolean isFull()
     {
-        if (isEmpty()) return false;
         return size == array.length;
     }
 
-    private void resize()
+    private void resize(int NewSize)
     {
-        T[] newArr = (T[]) new Object[array.length * 2];
+        T[] newArr = (T[]) new Object[NewSize];
         for (int i = 0; i < size; i++) {
             newArr[i] = array[(head + i) % array.length];
         }
@@ -26,22 +25,6 @@ public class ArrayDeque<T> implements Iterable<T> {
         tail = size;
     }
 
-    private void CutSize()
-    {
-        int newCapacity = array.length / 2;
-        if (newCapacity < 8) return;  // 通常避免缩得太小
-
-        T[] newArray = (T[]) new Object[newCapacity];
-
-        // 按照逻辑顺序重新拷贝
-        for (int i = 0; i < size; i++) {
-            newArray[i] = array[(head + i) % array.length];
-        }
-
-        array = newArray;
-        head = 0;
-        tail = size;
-    }
 
     public ArrayDeque()
     {
@@ -63,7 +46,7 @@ public class ArrayDeque<T> implements Iterable<T> {
 
     public void addFirst(T item)
     {
-        if(isFull()) resize();
+        if(isFull()) resize(array.length); * 2);
         head = (head - 1 + array.length) % array.length;
         array[head] = item;
         size++;
@@ -71,29 +54,35 @@ public class ArrayDeque<T> implements Iterable<T> {
 
     public void addLast(T item)
     {
-        if(isEmpty()) resize();
-        tail = (tail + 1) % array.length;
+        if(isFull()) resize(array.length); * 2);
         array[tail] = item;
+        tail = (tail + 1) % array.length;
         size++;
     }
 
     public T removeFirst()
     {
-        if(isEmpty()) throw new NoSuchElementException();
+        if(isEmpty()) return null;
         T value = array[head];
         array[head] = null;
         head = (head + 1 + array.length) % array.length;
         size--;
+        if (array.length >= 16 && size < array.length / 4) {
+            resize(array.length / 2);
+        }
         return value;
     }
 
     public T removeLast()
     {
-        if(isEmpty()) throw new NoSuchElementException();
+        if(isEmpty()) return null;
+        tail = (tail - 1 + array.length) % array.length;
         T value = array[tail];
         array[tail] = null;
-        tail = (tail - 1 + array.length) % array.length;
         size--;
+        if (array.length >= 16 && size < array.length / 4) {
+            resize(array.length / 2);
+        }
         return value;
     }
 
@@ -101,7 +90,7 @@ public class ArrayDeque<T> implements Iterable<T> {
     {
         if (isEmpty()) return null;
         if (index < 0 || index >= size) return null;
-        return array[index];
+        return array[(head + index) % array.length];
     }
 
     public void printDeque()
@@ -123,19 +112,24 @@ public class ArrayDeque<T> implements Iterable<T> {
     private class ArrayIterator implements Iterator<T>
     {
         int index;
+        int seen;
         public ArrayIterator()
         {
             index = head;
+            seen = 0;
         }
         @Override
         public boolean hasNext() {
-            return index != tail;
+            return seen < size;
         }
         @Override
         public T next()
         {
             if(!hasNext()) throw new NoSuchElementException();
-            return array[index++];
+            T value = array[index];
+            index = (index + 1) % array.length;
+            seen++;
+            return value;
         }
     }
 
@@ -143,16 +137,16 @@ public class ArrayDeque<T> implements Iterable<T> {
     public boolean equals(Object o)
     {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof ArrayDeque<?>)) return false;
         ArrayDeque<?> that = (ArrayDeque<?>) o;
         if (size != that.size()) return false;
         int NowHead = head;
         int OHead = that.head;
         for (int i = 0; i < size; i++)
         {
-            if (array[NowHead] != that.array[OHead]) return false;
+            if (!array[NowHead].equals(that.array[OHead])) return false;
             NowHead = (NowHead + 1 + array.length) % array.length;
-            OHead = (OHead + 1 + array.length) % array.length;
+            OHead = (OHead + 1 + that.array.length) % array.length;
         }
         return true;
     }
--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -5,207 +5,157 @@ import java.util.NoSuchElementException;
 
 public class LinkedListDeque<T> implements Iterable<T> {
     private Node sentinel;
-    private Node head;
-    private Node tail;
     private int size;
 
-
-
-    //初始化空的双端链表
-    public LinkedListDeque()
-    {
-        this.sentinel = new Node(null);
-        this.head = this.tail = null;
-        this.sentinel.next = null;
-        this.size = 0;
+    // 初始化空双向循环链表
+    public LinkedListDeque() {
+        sentinel = new Node(null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+        size = 0;
     }
 
-    //判断是否为空
-    public boolean isEmpty()
-    {
-        return this.size == 0;
+    public boolean isEmpty() {
+        return size == 0;
     }
 
-    //头插
-    public void addFirst(T item)
-    {
-        if (this.isEmpty())
-        {
-            this.head = this.tail = new Node(item);
-            return;
-        }
-        Node tempNode = new Node(item);
-        this.sentinel.next = tempNode;
-        tempNode.previous = this.sentinel;
-        tempNode.next = this.head;
-        this.head.previous = tempNode;
-        this.head = tempNode;
-        this.size++;
+    // 头插
+    public void addFirst(T item) {
+        Node newNode = new Node(item);
+
+        newNode.next = sentinel.next;
+        newNode.prev = sentinel;
+
+        sentinel.next.prev = newNode;
+        sentinel.next = newNode;
+
+        size++;
     }
 
-    //尾插
-    public void addLast(T item)
-    {
-        if (this.isEmpty())
-        {
-            this.tail = this.head = new Node(item);
-            return;
-        }
-        Node tempNode = new Node(item);
-        this.tail.next = tempNode;
-        tempNode.previous = this.tail;
-        tempNode.next = this.sentinel;
-        this.sentinel.previous = tempNode;
-        this.tail = tempNode;
-        this.size++;
+    // 尾插
+    public void addLast(T item) {
+        Node newNode = new Node(item);
+
+        newNode.prev = sentinel.prev;
+        newNode.next = sentinel;
+
+        sentinel.prev.next = newNode;
+        sentinel.prev = newNode;
+
+        size++;
     }
 
-    //大小获取
-    public int size()
-    {
-        return this.size;
+    public int size() {
+        return size;
     }
 
-    //打印
-    public void printDeque()
-    {
-        LinkedListDeque<T> tempList = this;
-        Node p =  tempList.head;
-        if (this.size == 0) return;
-        while (p != tempList.sentinel)
-        {
-            System.out.println(p.value + " ");
+    // 打印
+    public void printDeque() {
+        Node p = sentinel.next;
+        while (p != sentinel) {
+            System.out.print(p.value + " ");
             p = p.next;
         }
         System.out.println();
     }
 
-    //删除头上的
-    public T removeFirst()
-    {
-        if (this.isEmpty()) return null;
-        T value = this.head.value;
-        if (this.size == 1)
-        {
-            this.head = this.tail =null;
-            sentinel.previous = null;
-            sentinel.next = null;
-            size = 0; // 设置head和tail为sentinel
-        }
-        else
-        {
-            this.head = this.head.next;
-            this.head.previous = null;
-            this.sentinel.next = this.head;
-        }
-        this.size--;
+    // 删除头
+    public T removeFirst() {
+        if (isEmpty()) return null;
+
+        Node first = sentinel.next;
+        T value = first.value;
+
+        sentinel.next = first.next;
+        first.next.prev = sentinel;
+
+        size--;
         return value;
     }
 
-    public T removeLast()
-    {
-        if (this.isEmpty()) return null;
-        T value = this.tail.value;
-        if (this.size == 1) {
-            this.head = this.tail =null;
-            sentinel.previous = null;
-            sentinel.next = null;
-            size = 0;
-        } else {
-            this.tail = this.tail.previous;
-            this.tail.next = null;
-            this.sentinel.previous = this.tail;
-        }
-        this.size--;
+    // 删除尾
+    public T removeLast() {
+        if (isEmpty()) return null;
+
+        Node last = sentinel.prev;
+        T value = last.value;
+
+        sentinel.prev = last.prev;
+        last.prev.next = sentinel;
+
+        size--;
         return value;
     }
 
-    //查找
-    public T get(int index)
-    {
-        if (this.isEmpty()) return null;
-        if (index >= this.size || index < 0) return null;
-        Node p =  this.head;
-        for(int i = 0; i < index; i++)
-        {
+    // 非递归 get
+    public T get(int index) {
+        if (index < 0 || index >= size) return null;
+
+        Node p = sentinel.next;
+        for (int i = 0; i < index; i++) {
             p = p.next;
         }
         return p.value;
     }
 
-    //迭代器
-    public Iterator<T> iterator()
-    {
+    // 递归 get
+    public T getRecursive(int index) {
+        if (index < 0 || index >= size) return null;
+        return getRecursiveHelper(index, sentinel.next);
+    }
+
+    private T getRecursiveHelper(int index, Node node) {
+        if (index == 0) return node.value;
+        return getRecursiveHelper(index - 1, node.next);
+    }
+
+    // 迭代器
+    public Iterator<T> iterator() {
         return new LinkedListDequeIterator();
     }
 
-    private class LinkedListDequeIterator implements Iterator<T>
-    {
-        private Node NowHead;
+    private class LinkedListDequeIterator implements Iterator<T> {
+        private Node p = sentinel.next;
 
-        public  LinkedListDequeIterator()
-        {
-            NowHead = head;
-        }
-
-        @Override
         public boolean hasNext() {
-            return NowHead != null;
+            return p != sentinel;
         }
 
-        @Override
-        public T next()
-        {
+        public T next() {
             if (!hasNext()) throw new NoSuchElementException();
-            T value = NowHead.value;
-            NowHead = NowHead.next;
+            T value = p.value;
+            p = p.next;
             return value;
         }
     }
 
     @Override
-    public boolean equals(Object o)
-    {
+    public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || this.getClass() != o.getClass()) return false;
-        LinkedListDeque<?> that = (LinkedListDeque<?>) o;
-        LinkedListDeque<T> tempList = this;
-        LinkedListDeque<?>.Node TempOHead = that.head;
-        Node tempHead =  tempList.head;
-        if(that.size() != tempList.size()) return false;
-        for (int i = 0 ; i < that.size ; i++)
-        {
-            if (tempHead.getClass() != TempOHead.getClass()) return false;
-            if(!tempHead.value.equals(TempOHead.value)) return false;
-            tempHead = tempHead.next;
-            TempOHead = TempOHead.next;
+        if (!(o instanceof LinkedListDeque)) return false;
+
+        LinkedListDeque<?> other = (LinkedListDeque<?>) o;
+        if (this.size != other.size) return false;
+
+        Node p1 = this.sentinel.next;
+        LinkedListDeque<?>.Node p2 = other.sentinel.next;
+
+        while (p1 != this.sentinel) {
+            if (!p1.value.equals(p2.value)) return false;
+            p1 = p1.next;
+            p2 = p2.next;
         }
         return true;
     }
 
-    public T getRecursive(int index)
-    {
-        if (index >= this.size || index < 0) return null;
-        return getRecursiveHelper(index, this.head);
-    }
-
-    private T getRecursiveHelper(int index,Node tempNode)
-    {
-        if (index == 0)
-            return tempNode.value;
-        return getRecursiveHelper(index-1, tempNode.next);
-    }
-    //Node类实现
+    // Node 类
     private class Node {
-        private Node previous;
+        private Node prev;
         private Node next;
         private T value;
 
-        private Node(T val)
-        {
-            this.value = val;
-            this.previous = null;
-            this.next = null;
+        Node(T val) {
+            value = val;
         }
     }
 }
