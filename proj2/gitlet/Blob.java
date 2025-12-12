693d8682de206b968c5245c0073394c2ed18e0c9
Muzan Kibutsuji <2982287566@qq.com>
2025-12-12 20:03:19 +0800

    Commit:Add some instance variables and make some public methods to get them.

---
+++ proj2/gitlet/Blob.java
@@ -0,0 +1,9 @@
+package gitlet;
+
+/** Represents a gitlet blob object.
+
+ * Each blob contains:
+ * - Real file content (unzipped)
+ */
+public class Blob {
+}
--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -2,25 +2,66 @@ package gitlet;
 
 // TODO: any imports you need here
 
+import edu.princeton.cs.algs4.SET;
+
 import java.util.Date; // TODO: You'll likely use this in this class
+import java.util.HashMap;
 
 /** Represents a gitlet commit object.
- *  TODO: It's a good idea to give a description here of what else this Class
- *  does at a high level.
+
+ *  Each commit stores:
+ *  - A message
+ *  - A timestamp
+ *  - A mapping of file names to blob IDs
+ *  - A parent commit (and possibly a second parent for merges)
+ *  - A SHA1 ID
  *
- *  @author TODO
+ *  Commits form a DAG structure and are stored as serialized files in .gitlet/commits.
+ *  @author Noen
  */
 public class Commit {
     /**
      * TODO: add instance variables here.
-     *
+
      * List all instance variables of the Commit class here with a useful
      * comment above them describing what that variable represents and how that
      * variable is used. We've provided one example for `message`.
      */
-
-    /** The message of this Commit. */
-    private String message;
+    private String meassage;
+    private String timestamp;
+    private HashMap<String, Blob> Blobs; //use different SHA1ID to represent each blob.
+    private Commit Parent;
 
     /* TODO: fill in the rest of this class. */
+
+    public Commit(String meassage, Commit ParentCommit)
+    {
+     this.meassage = meassage;
+     this.Parent = ParentCommit;
+     this.timestamp = new Date().toString();
+     if (ParentCommit!=null)
+     {
+         this.timestamp="Whatever it is just let it be here.";
+     }
+    }
+
+    public String getMeassage()
+    {
+        return meassage;
+    }
+
+    public String getTimestamp()
+    {
+        return timestamp;
+    }
+
+    public HashMap<String, Blob> getBlobs()
+    {
+        return Blobs;
+    }
+
+    public Commit getParent()
+    {
+        return Parent;
+    }
 }
